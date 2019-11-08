580acf67641485d90dceaf5f14cc4a5dcb85cdfb
chengzhen <cheng1992zhen@163.com>
2019-11-08 18:19:38 +0800

    mainActivity

--- app/src/main/java/com/example/kotlindemo/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/ui/activity/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.kotlindemo.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.view.KeyEvent
 import com.example.kotlindemo.R
+import com.example.kotlindemo.base.BaseActivity
 import com.example.kotlindemo.util.showToast
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    override fun onClick(v: View?) {
-        showToast("toast")
+class MainActivity : BaseActivity() {
+    private val mTitles = arrayOf("每日精选", "发现", "热门", "我的")
+
+    // 未被选中的图标
+    private val mIconUnSelectIds = intArrayOf(R.mipmap.ic_home_normal, R.mipmap.ic_discovery_normal, R.mipmap.ic_hot_normal, R.mipmap.ic_mine_normal)
+    // 被选中的图标
+    private val mIconSelectIds = intArrayOf(R.mipmap.ic_home_selected, R.mipmap.ic_discovery_selected, R.mipmap.ic_hot_selected, R.mipmap.ic_mine_selected)
+
+
+    override fun layoutId(): Int {
+        return R.layout.activity_main
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        tv_content.setOnClickListener(this)
+
+    }
+
+    override fun initView() {
+
+
+    }
+
+    override fun initData() {
+
+
+    }
+
+    private var mExitTime: Long = 0
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (System.currentTimeMillis().minus(mExitTime) <= 2000) {
+                finish()
+            } else {
+                mExitTime = System.currentTimeMillis()
+                showToast("再按一次退出程序")
+            }
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
---
+++ app/src/main/java/com/example/kotlindemo/ui/fragment/MineFragment.kt
@@ -0,0 +1,17 @@
+package com.example.kotlindemo.ui.fragment
+
+import com.example.kotlindemo.base.BaseFragment
+
+class MineFragment : BaseFragment() {
+    override fun getLayoutId(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun lazyLoad() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
