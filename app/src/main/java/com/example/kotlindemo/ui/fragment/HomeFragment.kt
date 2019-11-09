618576483e8c276e37f45ed5412a2c40fbee1e79
chengzhen <cheng1992zhen@163.com>
2019-11-09 18:02:06 +0800

    update

--- app/src/main/java/com/example/kotlindemo/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/kotlindemo/ui/fragment/HomeFragment.kt
@@ -1,8 +1,32 @@
 package com.example.kotlindemo.ui.fragment
 
+import android.os.Bundle
 import com.example.kotlindemo.base.BaseFragment
+import com.example.kotlindemo.mvp.contract.HomeContract
+import com.example.kotlindemo.mvp.model.bean.HomeBean
 
-class HomeFragment : BaseFragment() {
+class HomeFragment : BaseFragment() ,HomeContract.View{
+    override fun setHomeData(homeBean: HomeBean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setMoreData(itemList: ArrayList<HomeBean.Issue.Item>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showError(msg: String, errorCode: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun dismissLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private var mTitle: String? = null
     override fun getLayoutId(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -14,4 +38,15 @@ class HomeFragment : BaseFragment() {
     override fun lazyLoad() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    companion object {
+        fun getInstance(title: String): HomeFragment {
+            val homeFragment = HomeFragment()
+            homeFragment.arguments = Bundle()
+            homeFragment.mTitle = title
+            return homeFragment
+        }
+    }
+
+
 }
